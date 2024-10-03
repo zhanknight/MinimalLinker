6b7aed5078715505468016d6876b2294e0ce8cce
zhanknight <20487008+zhanknight@users.noreply.github.com>
2024-10-03 16:58:47 -0400

    Add redirect endpoint

--- MinimalLinker/Program.cs
+++ MinimalLinker/Program.cs
@@ -6,21 +6,34 @@ using MinimalLinker;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<LinksContext>
-    (o => o.UseSqlite(builder.Configuration.GetConnectionString("LinksConnection")));
+    (o => o.UseSqlite(builder.Configuration.GetConnectionString("LinksContext")));
 
 var app = builder.Build();
 
-app.MapGet("/{id}", async Task<Results<NotFound, Ok<Link>>>
+// need an endpoint that returns the link object itself
+// need an endpoint that interprets and redirects to original link
+// need an endpoint that accepts POSTs of new links to be minimized
+
+// Return the link object
+app.MapGet("/links/{id}", async Task<Results<NotFound, Ok<Link>>>
     ([FromRoute] string id, LinksContext context) =>
     {
         Link? link = await context.Links.FindAsync(id);
 
-        if (link == null)
-        {
-            return TypedResults.NotFound();
-        }
+        if (link == null) return TypedResults.NotFound();
         
         return TypedResults.Ok<Link>(link);
+    });
+
+// Redirect from short link to original url
+app.MapGet("/{id}", async Task<Results<NotFound, RedirectHttpResult>>
+    ([FromRoute] string id, LinksContext context) =>
+    {
+        Link? link = await context.Links.FindAsync(id);
+        
+        if (link == null) return TypedResults.NotFound();
+        
+        return TypedResults.Redirect(link.fullUrl);
     });
 
 app.Run();

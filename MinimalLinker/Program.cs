28c62a9127b0ecd554476512acc4dd6f8cbe3193
zhanknight <20487008+zhanknight@users.noreply.github.com>
2024-10-06 13:51:05 -0400

    Add Authentication, Authorization, and SwaggerUI

--- MinimalLinker/Program.cs
+++ MinimalLinker/Program.cs
@@ -1,12 +1,52 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using MinimalLinker;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<LinksContext>
     (o => o.UseSqlite(builder.Configuration.GetConnectionString("LinksContext")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(x => 
+    { 
+        x.Authority = "https://localhost"; 
+        x.Audience = "MinimalLinker";
+    });
+builder.Services.AddAuthorization();
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("RequireCreate", policy => policy.RequireClaim("permission", "create"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.AddSecurityDefinition("jwt",
+        new()
+        {
+            Name = "Authorization",
+            Description = "JWT Bearer Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            In = ParameterLocation.Header
+        });
+    o.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "jwt"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -38,10 +78,23 @@ app.MapPost("/", async Task<Results<BadRequest, CreatedAtRoute<Link>>>
     { 
         // add validation to input here
         string newId = UrlService.GenerateShortUrl(newUrl);
-        Link newLink = new Link { fullUrl = newUrl, id = UrlService.GenerateShortUrl(newId) };
-        // current method has a small chance for short hash / id collisions, should add checks.
-        var created = await context.Links.AddAsync(newLink);
-        return TypedResults.CreatedAtRoute(created.Entity, "ResolveShortLink", new { id = created.Entity.id }); 
-    });
+        var exists = await context.Links.FindAsync(newId);
+        if (exists == null)
+        {
+            Link newLink = new Link { fullUrl = newUrl, id = UrlService.GenerateShortUrl(newId) };
+            var created = await context.Links.AddAsync(newLink);
+            return TypedResults.CreatedAtRoute(created.Entity, "ResolveShortLink", new { id = created.Entity.id }); 
+        }
+        // If we have a short has collision, just return bad request for now - handle iterations in future update
+        return TypedResults.BadRequest();
+    }).RequireAuthorization("RequireCreate");
+
+app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
